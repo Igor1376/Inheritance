726cf0a707ca0090f2e82f07d137332bb436b270
Igor <bignick13@yandex.ru>
2024-06-28 21:17:18 +0300

    Write to file allignment done

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -12,6 +12,10 @@ using namespace std;
 
 class Human
 {
+    static const int HUMAN_TYPE_WIDTH = 15;
+    static const int LAST_NAME_WIDTH = 15;
+    static const int FIRST_NAME_WIDTH = 15;
+    static const int AGE_WIDTH = 5;
     std::string last_name;
     std::string first_name;
     unsigned int age;
@@ -62,18 +66,37 @@ public:
         return os << last_name << " " << first_name << " " << age << " y/o ";
 
     }
+    virtual std::ofstream& info(std::ofstream& ofs)const
+    {
+        //ofs << strchr(typeid(*this).name(), ' ') + 1 << ":\t " << last_name << first_name << " " << age;
+        ofs.width(HUMAN_TYPE_WIDTH); ofs << left << std::string(strchr(typeid(*this).name(), ' ') + 1) + ":";
+        ofs.width(LAST_NAME_WIDTH); ofs << left << last_name;
+        ofs.width(FIRST_NAME_WIDTH); ofs << left << first_name;
+        ofs.width(AGE_WIDTH); ofs << left <<  age;
+        return ofs;
+     }
+
 };
 
 std::ostream& operator <<(std::ostream& os, const Human& obj)
 {
     return obj.info(os);
 }
+std::ofstream& operator <<(std::ofstream& ofs, const Human& obj)
+{
+    return obj.info(ofs);
+}
+
 
 #define STUDENT_TAKE_PARAMETERS const std ::string& speciality, const std ::string& group, double rating, double attendance
 #define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
 
 class Student :public Human
 {
+    static const int SPECIALITY_WIDTH = 25;
+    static const int GROUP_WIDTH = 8;
+    static const int RATING_WIDTH = 8;
+    static const int ATTENDANCE_WIDTH = 8;
     std::string speciality;
     std::string group;
     double rating;
@@ -141,10 +164,21 @@ public:
         Human::info(os);
         return Human::info(os) << " " << speciality << " " << group << " " << rating << " " << attendance;
     }
+    std::ofstream& info(std::ofstream& ofs)const override
+    {
+        Human::info(ofs);
+        ofs.width(SPECIALITY_WIDTH); ofs << speciality;
+        ofs.width(GROUP_WIDTH); ofs << group;
+        ofs.width(RATING_WIDTH); ofs << rating;
+        ofs.width(ATTENDANCE_WIDTH); ofs << attendance;
+        return ofs;
+    }
 };
 
 class Teacher : public Human
 {
+    static const int SPECIALITY_WIDTH = 25;
+    static const int EXPERIENCE_WIDTH = 25;
     std::string speciality;
     unsigned int experience;
 public:
@@ -184,10 +218,18 @@ public:
     {
         return Human::info(os) << " " << speciality << " " << experience << " " << "years";
     }
+    std::ofstream& info(std::ofstream& ofs)const override
+    {
+        Human::info(ofs);
+        ofs.width(SPECIALITY_WIDTH); ofs << speciality;
+        ofs.width(EXPERIENCE_WIDTH); ofs << experience;
+        return ofs;
+    }
 };
 
 class Graduate :public Student
 {
+    static const int SUBJECT_WIDTH = 32;
     std::string subject;
 public:
     const std::string& get_subject()const
@@ -217,6 +259,12 @@ public:
     std::ostream& info(std::ostream& os) const override
     {
         return Student::info(os) << " " << subject;
+    }
+    std::ofstream& info(std::ofstream& ofs) const override
+    {
+        Student::info(ofs);
+        ofs.width(SUBJECT_WIDTH); ofs << subject;
+        return ofs;
     }
 };
 
@@ -273,7 +321,8 @@ void main()
       new Student("Pinkam", "Jessie", 22, "Chemistry", "WW_220", 70, 97),
       new Teacher("White", "Walter", 50, "Chemistry", 25),
       new Graduate("Schreder", "Hank", 40, "Criminalistic", "OBN", 80, 90, "How to catch Heisenberg"),
-      new Student("Vercetty", "Tommy", 30, "Theft", "Vice" ,97, 98)
+      new Student("Vercetty", "Tommy", 30, "Theft", "Vice" ,97, 98),
+      new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
     };
 
     Print(group, sizeof(group) / (sizeof(group)[0]));
